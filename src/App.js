c0069b984fc2bdef7a6559ebb4fde5fe2a014b81
tomdepta <tom.depta@gmail.com>
2018-11-21 20:06:34 +0100

    read my details from json file

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import PersonalDetails from "./blocks/personal-details";
 import SkillsContainer from "./components/skills-container";
 import ProjectList from "./components/project-list";
 import Interests from "./blocks/interests";
+import myDetails from "./my-details";
 
 import "./App.scss";
 
@@ -32,27 +33,19 @@ class App extends Component {
                 <Divider />
                 <SkillsContainer
                   title="Languages"
-                  skills={myDetails2.languages}
+                  skills={myDetails.languages}
                 />
                 <Divider />
                 <SkillsContainer
                   title="Programming Skills"
-                  skills={myDetails2.programmingSkills}
+                  skills={myDetails.programmingSkills}
                 />
               </Sticky>
             </div>
           </aside>
           <section ref={this.handleContextRef}>
             <Container text>
-              <p>
-                Na co dzień Senior Web Developer, ale mówi o sobie Inżynier
-                Oprogramowania, bo liczy się dostarczanie wartości, nie ważne
-                czy w grę wchodzi kod czy nie. Od 6 lat pracuje w firmie Future
-                Processing, po godzinach z pasją rozwijając swoje umiejętności w
-                hobbystycznych projektach. Frontend to dla niego chleb
-                powszedni, z Angularem idzie za pan brat od wersji 1.2 po
-                dzisiejszy Angular 2+, zdradzając go czasem dla Reacta.
-              </p>
+              <p>{myDetails.aboutMe}</p>
               <Divider />
               <Timeline
                 title="Professional experience"
@@ -83,68 +76,69 @@ class App extends Component {
 }
 
 export default App;
-const myDetails2 = {
-  languages: [
-    {
-      name: "English",
-      rating: 4
-    },
-    {
-      name: "German",
-      rating: 3
-    }
-  ],
-  programmingSkills: [
-    {
-      name: "C#",
-      rating: 4
-    },
-    {
-      name: "JavaScript",
-      rating: 3
-    },
-    {
-      name: "OOP",
-      rating: 3
-    }
-  ]
-};
-const myDetails = {
-  githubAccountName: "kszymanski",
-  professionalExperience: [
-    {
-      from: "2015",
-      to: "present",
-      description: "Senior .NET developer at Future Processing"
-    },
-    {
-      from: "2014",
-      to: "2015",
-      description: ".NET developer at Programex"
-    },
-    {
-      from: "2010",
-      to: "2014",
-      description: "Junior .NET developer at SoftPol"
-    }
-  ],
-  educationHistory: [
-    {
-      from: "2011",
-      to: "2014",
-      description: "Silesian University of Technology - Computer Science"
-    },
-    {
-      from: "2008",
-      to: "2011",
-      description: "High School A. Frycza-Modrzewskiego in Rybnik"
-    }
-  ],
-  otherActivities: [
-    {
-      from: "2013",
-      to: "present",
-      description: "Writing and maintaning a blog at www.elo.pl"
-    }
-  ]
-};
+
+// const myDetails = {
+//   aboutMe:
+//     "Na co dzień Senior Web Developer, ale mówi o sobie Inżynier Oprogramowania, bo liczy się dostarczanie wartości, nie ważne czy w grę wchodzi kod czy nie. Od 6 lat pracuje w firmie Future Processing, po godzinach z pasją rozwijając swoje umiejętności w hobbystycznych projektach. Frontend to dla niego chleb powszedni, z Angularem idzie za pan brat od wersji 1.2 po dzisiejszy Angular 2+, zdradzając go czasem dla Reacta.",
+//   languages: [
+//     {
+//       name: "English",
+//       rating: 4
+//     },
+//     {
+//       name: "German",
+//       rating: 3
+//     }
+//   ],
+//   programmingSkills: [
+//     {
+//       name: "C#",
+//       rating: 4
+//     },
+//     {
+//       name: "JavaScript",
+//       rating: 3
+//     },
+//     {
+//       name: "OOP",
+//       rating: 3
+//     }
+//   ],
+//   githubAccountName: "kszymanski",
+//   professionalExperience: [
+//     {
+//       from: "2015",
+//       to: "present",
+//       description: "Senior .NET developer at Future Processing"
+//     },
+//     {
+//       from: "2014",
+//       to: "2015",
+//       description: ".NET developer at Programex"
+//     },
+//     {
+//       from: "2010",
+//       to: "2014",
+//       description: "Junior .NET developer at SoftPol"
+//     }
+//   ],
+//   educationHistory: [
+//     {
+//       from: "2011",
+//       to: "2014",
+//       description: "Silesian University of Technology - Computer Science"
+//     },
+//     {
+//       from: "2008",
+//       to: "2011",
+//       description: "High School A. Frycza-Modrzewskiego in Rybnik"
+//     }
+//   ],
+//   otherActivities: [
+//     {
+//       from: "2013",
+//       to: "present",
+//       description: "Writing and maintaning a blog at www.elo.pl"
+//     }
+//   ]
+// };
